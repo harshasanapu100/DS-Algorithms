25e096e47654ad4f3aec4da25a6bb2d360a14809
Harsha Vardhan Reddy <harshasanapu100@gmail.com>
2021-04-08 08:21:56 +0530

    Implement Add First, Add Last and Add At methods of Custom Linked List

--- ConsoleApp/DSSingleLinkedList.cs
+++ ConsoleApp/DSSingleLinkedList.cs
@@ -7,6 +7,23 @@ namespace ConsoleApp
     {
         static void Main(string[] args)
         {
+            SingleLinkedList singleLinkedList = new SingleLinkedList();
+            Console.WriteLine("Effect of Add First (30,20, 10)");
+            singleLinkedList.AddFirst(30);
+            singleLinkedList.AddFirst(20);
+            singleLinkedList.AddFirst(10);
+            singleLinkedList.Print();
+
+            Console.WriteLine();
+            Console.WriteLine("Effect of Add Last(40,50)");
+            singleLinkedList.AddLast(40);
+            singleLinkedList.AddLast(50);
+            singleLinkedList.Print();
+
+            Console.WriteLine();
+            Console.WriteLine("Effect of Add At position(3) value(25)");
+            singleLinkedList.AddAt(25,3);
+            singleLinkedList.Print();
 
             Console.ReadKey();
         }
--- CustomLibrary/SingleLinkedList.cs
+++ CustomLibrary/SingleLinkedList.cs
@@ -4,12 +4,13 @@ namespace CustomLibrary
 {
     public class SingleLinkedList
     {
+        #region private fields
         private class Node
         {
             public int value;
             public Node next;
 
-            Node(int value)
+            public Node(int value)
             {
                 this.value = value;
             }
@@ -19,15 +20,107 @@ namespace CustomLibrary
         private Node tail;
         private int length;
 
+        #endregion
+
+        #region public methods
         public void Print()
         {
             var current = head;
 
-            while(current!= null)
+            while (current != null)
             {
                 Console.WriteLine(current.value);
                 current = current.next;
             }
         }
+
+        public void AddFirst(int item)
+        {
+            Node node = new Node(item);
+
+            if (IsEmpty())
+            {
+                head = tail = node;
+            }
+            else
+            {
+                node.next = head;
+                head = node;
+            }
+
+            length++;
+        }
+
+        public void AddLast(int item)
+        {
+            Node node = new Node(item);
+
+            if (IsEmpty())
+            {
+                head = tail = node;
+            }
+            else
+            {
+                tail.next = node;
+                tail = node;
+            }
+
+            length++;
+        }
+
+        public void AddAt(int item, int position)
+        {
+            Node node = new Node(item);
+
+            if (IsEmpty())
+            {
+                head = tail = node;
+            }
+            else
+            {
+                if (position < 1)
+                {
+                    throw new ArgumentException("Position should be >= 1");
+                }
+                else if(position == 1)
+                {
+                    node.next = head;
+                    head = node;
+                }
+                else
+                {
+                    var current = head;
+
+                    for (int i = 1; i < position - 1; i++)
+                    {
+
+                        if (current != null)
+                        {
+                            current = current.next;
+                        }
+                    }
+
+                    if(current!= null)
+                    {
+                        node.next = current.next;
+                        current.next = node;
+                    }
+                    else
+                    {
+                        Console.WriteLine("The previous node is null.");
+                    }
+                }
+            }
+
+            length++;
+        }
+        #endregion
+
+        #region private methods
+        private bool IsEmpty()
+        {
+            return head == null;
+        }
+        #endregion
     }
 }
