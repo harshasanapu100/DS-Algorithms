d33ef50ca18300dbe27a70b7353003efee18bc57
Harsha Vardhan Reddy <harshasanapu100@gmail.com>
2021-04-18 20:27:30 +0530

    Add Print and Insert methods for DynamicArray class

--- ConsoleApp/DSDynamicArray.cs
+++ ConsoleApp/DSDynamicArray.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using CustomLibrary;
 
 namespace ConsoleApp
 {
@@ -8,7 +9,45 @@ namespace ConsoleApp
     {
         static void Main(string[] args)
         {
+            DynamicArray dynamicArray = new DynamicArray(5);
 
+            Console.WriteLine("Array created with 5 elements");
+            Console.WriteLine("Adding below 5 elements to the array");
+            Console.WriteLine("10, 20, 30, 40, 50");
+            dynamicArray.Insert(10);
+            dynamicArray.Insert(20);
+            dynamicArray.Insert(30);
+            dynamicArray.Insert(40);
+            dynamicArray.Insert(50);
+
+            Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Printing the elements");
+            Console.ForegroundColor = ConsoleColor.White;
+            dynamicArray.Print();
+
+            Console.WriteLine();
+            Console.WriteLine("Adding below element as 6th element to the array");
+            Console.WriteLine("60");
+            Console.WriteLine();
+            dynamicArray.Insert(60);
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Printing the elements");
+            Console.ForegroundColor = ConsoleColor.White;
+            dynamicArray.Print();
+
+            Console.WriteLine();
+            Console.WriteLine("Adding below 2 more elements to the array");
+            Console.WriteLine("70, 80");
+            Console.WriteLine();
+            dynamicArray.Insert(70);
+            dynamicArray.Insert(80);
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Printing the elements");
+            Console.ForegroundColor = ConsoleColor.White;
+            dynamicArray.Print();
+
+            Console.ReadLine();
         }
     }
 }
--- CustomLibrary/DynamicArray.cs
+++ CustomLibrary/DynamicArray.cs
@@ -6,6 +6,49 @@ namespace CustomLibrary
 {
     public class DynamicArray
     {
+        #region private fileds
+        private int[] items;
+        private int count;
+        #endregion
 
+        #region constructor
+        public DynamicArray(int length)
+        {
+            items = new int[length];
+        }
+        #endregion
+
+        #region public methods
+        public void Print()
+        {
+            for (int i = 0; i < count; i++)
+            {
+                Console.WriteLine(items[i]);
+            }
+        }
+
+        public void Insert(int item)
+        {
+            if (items.Length == count)
+            {
+                int[] newItems = new int[count * 2];
+
+                for (int i = 0; i < count; i++)
+                {
+                    newItems[i] = items[i];
+                }
+
+                items = newItems;
+            }
+
+            items[count] = item;
+
+            count++;
+        }
+        #endregion
+
+        #region private methods
+
+        #endregion
     }
 }
