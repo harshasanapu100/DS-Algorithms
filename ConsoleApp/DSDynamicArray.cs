99ee371b5411472831ba1f555e16837abc46fac6
Harsha Vardhan Reddy <harshasanapu100@gmail.com>
2021-04-19 07:49:08 +0530

    Add RemoveAt, IndexOf, Max, Min, Intersect, Reverse, InsertAt methods to DynamicArrays class

--- ConsoleApp/DSDynamicArray.cs
+++ ConsoleApp/DSDynamicArray.cs
@@ -9,45 +9,126 @@ namespace ConsoleApp
     {
         static void Main(string[] args)
         {
-            DynamicArray dynamicArray = new DynamicArray(5);
+            try
+            {
+                DynamicArray dynamicArray = new DynamicArray(5);
 
-            Console.WriteLine("Array created with 5 elements");
-            Console.WriteLine("Adding below 5 elements to the array");
-            Console.WriteLine("10, 20, 30, 40, 50");
-            dynamicArray.Insert(10);
-            dynamicArray.Insert(20);
-            dynamicArray.Insert(30);
-            dynamicArray.Insert(40);
-            dynamicArray.Insert(50);
+                Console.WriteLine("Array created with 5 elements");
+                Console.WriteLine("Adding below 5 elements to the array");
+                Console.WriteLine("10, 20, 30, 40, 50");
+                dynamicArray.Insert(10);
+                dynamicArray.Insert(20);
+                dynamicArray.Insert(30);
+                dynamicArray.Insert(40);
+                dynamicArray.Insert(50);
 
-            Console.WriteLine();
-            Console.ForegroundColor = ConsoleColor.Green;
-            Console.WriteLine("Printing the elements");
-            Console.ForegroundColor = ConsoleColor.White;
-            dynamicArray.Print();
+                Console.WriteLine();
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Printing the elements");
+                Console.ForegroundColor = ConsoleColor.White;
+                dynamicArray.Print();
 
-            Console.WriteLine();
-            Console.WriteLine("Adding below element as 6th element to the array");
-            Console.WriteLine("60");
-            Console.WriteLine();
-            dynamicArray.Insert(60);
-            Console.ForegroundColor = ConsoleColor.Green;
-            Console.WriteLine("Printing the elements");
-            Console.ForegroundColor = ConsoleColor.White;
-            dynamicArray.Print();
+                Console.WriteLine();
+                Console.WriteLine("Adding below element as 6th element to the array");
+                Console.WriteLine("60");
+                Console.WriteLine();
+                dynamicArray.Insert(60);
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Printing the elements");
+                Console.ForegroundColor = ConsoleColor.White;
+                dynamicArray.Print();
 
-            Console.WriteLine();
-            Console.WriteLine("Adding below 2 more elements to the array");
-            Console.WriteLine("70, 80");
-            Console.WriteLine();
-            dynamicArray.Insert(70);
-            dynamicArray.Insert(80);
-            Console.ForegroundColor = ConsoleColor.Green;
-            Console.WriteLine("Printing the elements");
-            Console.ForegroundColor = ConsoleColor.White;
-            dynamicArray.Print();
+                Console.WriteLine();
+                Console.WriteLine("Adding below 2 more elements to the array");
+                Console.WriteLine("70, 80");
+                Console.WriteLine();
+                dynamicArray.Insert(70);
+                dynamicArray.Insert(80);
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Printing the elements");
+                Console.ForegroundColor = ConsoleColor.White;
+                dynamicArray.Print();
 
-            Console.ReadLine();
+                Console.WriteLine();
+                Console.WriteLine("Removing array element at index(2) item(30)");
+                dynamicArray.RemoveAt(2);
+                Console.WriteLine();
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Printing the elements");
+                Console.ForegroundColor = ConsoleColor.White;
+                dynamicArray.Print();
+                Console.WriteLine();
+
+                Console.WriteLine("Finding the index of item(40)");
+                int index40 = dynamicArray.IndexOf(40);
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Index of 40 is {0}", index40);
+                Console.ForegroundColor = ConsoleColor.White;
+
+                Console.WriteLine();
+                Console.WriteLine("Finding the index of item(30) which is not available in array");
+                int index30 = dynamicArray.IndexOf(30);
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Index of 30 is {0}", index30);
+                Console.ForegroundColor = ConsoleColor.White;
+
+                Console.WriteLine();
+                Console.WriteLine("Finding the maximum value in array");
+                int max = dynamicArray.Max();
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Maximum value in array is: {0}", max);
+                Console.ForegroundColor = ConsoleColor.White;
+
+                Console.WriteLine();
+                Console.WriteLine("Finding the minimum value in array");
+                int min = dynamicArray.Min();
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Minimum value in array is: {0}", min);
+                Console.ForegroundColor = ConsoleColor.White;
+
+                Console.WriteLine();
+                Console.WriteLine("Perform Intersecting with below other array");
+                Console.WriteLine("Other array contains 20, 80, 90, 50, 30");
+                DynamicArray otherArray = new DynamicArray(5);
+                otherArray.Insert(20);
+                otherArray.Insert(80);
+                otherArray.Insert(90);
+                otherArray.Insert(50);
+                otherArray.Insert(30);
+                DynamicArray intersect = dynamicArray.Intersect(otherArray);
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Printing the elements");
+                Console.ForegroundColor = ConsoleColor.White;
+                intersect.Print();
+
+                Console.WriteLine();
+                Console.WriteLine("Inserting new item at index(2) value(30)");
+                dynamicArray.InsertAt(30,2);
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Printing the elements");
+                Console.ForegroundColor = ConsoleColor.White;
+                dynamicArray.Print();
+
+                Console.WriteLine();
+                Console.WriteLine("Reversing the array");
+                dynamicArray.Reverse();
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Printing the elements");
+                Console.ForegroundColor = ConsoleColor.White;
+                dynamicArray.Print();
+
+                Console.WriteLine();
+                Console.WriteLine("Removing array element at index(-1)");
+                dynamicArray.RemoveAt(-1);
+
+                Console.ReadLine();
+            }
+            catch (Exception ex)
+            {
+                Console.ForegroundColor = ConsoleColor.DarkRed;
+                Console.WriteLine(ex.Message);
+                Console.ForegroundColor = ConsoleColor.White;
+            }
         }
     }
 }
--- CustomLibrary/DynamicArray.cs
+++ CustomLibrary/DynamicArray.cs
@@ -29,6 +29,149 @@ namespace CustomLibrary
 
         public void Insert(int item)
         {
+            ResizeIfRequired();
+
+            items[count] = item;
+
+            count++;
+        }
+
+        public void RemoveAt(int index)
+        {
+            if (IsEmpty())
+            {
+                throw new ArgumentException("Array is empty");
+            }
+
+            if (index < -1 || index >= count)
+            {
+                throw new ArgumentException();
+            }
+
+            for (int i = index; i < count; i++)
+            {
+                items[i] = items[i + 1];
+            }
+
+            count--;
+        }
+
+        public int IndexOf(int item)
+        {
+            for (int i = 0; i < count; i++)
+            {
+                if (items[i] == item)
+                {
+                    return i;
+                }
+            }
+
+            return -1;
+        }
+
+        public int Max()
+        {
+            if (IsEmpty())
+            {
+                throw new ArgumentException("Array is empty");
+            }
+
+            int maximum = items[0];
+
+            for (int i = 1; i < count; i++)
+            {
+                if (items[i] > maximum)
+                {
+                    maximum = items[i];
+                }
+            }
+            return maximum;
+        }
+
+        public int Min()
+        {
+            if (IsEmpty())
+            {
+                throw new ArgumentException("Array is empty");
+            }
+
+            int minimum = items[0];
+
+            for (int i = 1; i < count; i++)
+            {
+                if (items[i] < minimum)
+                {
+                    minimum = items[i];
+                }
+            }
+            return minimum;
+        }
+
+        public DynamicArray Intersect(DynamicArray anotherArray)
+        {
+            if (IsEmpty())
+            {
+                throw new ArgumentException("Array is empty");
+            }
+
+            DynamicArray newArray = new DynamicArray(count);
+
+            for (int i = 0; i < anotherArray.count; i++)
+            {
+                if (IndexOf(anotherArray.items[i]) > -1)
+                {
+                    newArray.Insert(anotherArray.items[i]);
+                }
+            }
+
+            return newArray;
+        }
+
+        public void Reverse()
+        {
+            if (IsEmpty())
+            {
+                throw new ArgumentException("Array is empty");
+            }
+
+            DynamicArray reverseArray = new DynamicArray(count);
+
+            for (int i = count - 1; i > -1; i--)
+            {
+                reverseArray.Insert(items[i]);
+            }
+
+            items = reverseArray.items;
+        }
+
+        public void InsertAt(int item, int index)
+        {
+            if (index < 0 || index > count)
+            {
+                throw new ArgumentException();
+            }
+
+            ResizeIfRequired();
+
+            for (int i = count - 1; i >= index; i--)
+            {
+                items[i + 1] = items[i];
+            }
+
+            items[index] = item;
+
+            count++;
+        }
+        #endregion
+
+        #region private methods
+        private bool IsEmpty()
+        {
+            return count <= 0;
+        }
+
+        private void ResizeIfRequired()
+        {
             if (items.Length == count)
             {
                 int[] newItems = new int[count * 2];
@@ -40,15 +183,7 @@ namespace CustomLibrary
 
                 items = newItems;
             }
-
-            items[count] = item;
-
-            count++;
         }
-        #endregion
-
-        #region private methods
-
         #endregion
     }
 }
