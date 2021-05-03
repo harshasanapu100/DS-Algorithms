9484bfec1b0b2276287acbca57de92fca5b5209e
Harsha Vardhan Reddy <harshasanapu100@gmail.com>
2021-05-03 20:53:56 +0530

    Add Circular Queue using Array with popular methods and Reversing the queue using stack

--- ConsoleApp/DSQueueArray.cs
+++ ConsoleApp/DSQueueArray.cs
@@ -10,6 +10,74 @@ namespace ConsoleApp
         {
             try
             {
+                Console.ForegroundColor = ConsoleColor.DarkYellow;
+                Console.WriteLine("Implementing the circular queue with Array");
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Pushing the 10, 20, 30, 40, 50 elements to queue");
+                Console.ForegroundColor = ConsoleColor.White;
+
+                CustomQueueArray customQueueArray = new CustomQueueArray(5);
+                customQueueArray.Enqueue(10);
+                customQueueArray.Enqueue(20);
+                customQueueArray.Enqueue(30);
+                customQueueArray.Enqueue(40);
+                customQueueArray.Enqueue(50);
+
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Printing the elements");
+                Console.ForegroundColor = ConsoleColor.White;
+                customQueueArray.Print();
+
+                Console.WriteLine();
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Removing the element from queue");
+                Console.ForegroundColor = ConsoleColor.White;
+                int front = customQueueArray.Dequeue();
+                Console.WriteLine("Removed Item is: {0}", front);
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Printing the elements");
+                Console.ForegroundColor = ConsoleColor.White;
+                customQueueArray.Print();
+
+                Console.WriteLine();
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Removing one more element from queue");
+                Console.ForegroundColor = ConsoleColor.White;
+                front = customQueueArray.Dequeue();
+                Console.WriteLine("Removed Item is: {0}", front);
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Printing the elements");
+                Console.ForegroundColor = ConsoleColor.White;
+                customQueueArray.Print();
+
+                Console.WriteLine();
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Adding new item as 11");
+                Console.ForegroundColor = ConsoleColor.White;
+                customQueueArray.Enqueue(11);
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Printing the elements");
+                Console.ForegroundColor = ConsoleColor.White;
+                customQueueArray.Print();
+
+                Console.WriteLine();
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Adding new item as 22");
+                Console.ForegroundColor = ConsoleColor.White;
+                customQueueArray.Enqueue(22);
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Printing the elements");
+                Console.ForegroundColor = ConsoleColor.White;
+                customQueueArray.Print();
+
+                Console.WriteLine();
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Peeking the element");
+                Console.ForegroundColor = ConsoleColor.White;
+                front = customQueueArray.Peek();
+                Console.WriteLine("Peek item is: {0}", front);
+                Console.ForegroundColor = ConsoleColor.Green;
                 Console.ReadKey();
             }
             catch (Exception ex)
--- CustomLibrary/CustomQueueArray.cs
+++ CustomLibrary/CustomQueueArray.cs
@@ -4,8 +4,99 @@ using System.Text;
 
 namespace CustomLibrary
 {
-    class CustomQueueArray
+    public class CustomQueueArray
     {
+        #region private fileds
+        private int[] items;
+        private int front;
+        private int rear;
+        private int count;
+        private int incr;
+        #endregion
+
+        #region constructor
+        public CustomQueueArray(int length)
+        {
+            this.items = new int[length];
+        }
+        #endregion
+
+        #region public methods
+        public void Enqueue(int item)
+        {
+            if (count == items.Length)
+            {
+                throw new Exception("Queue is overflow");
+            }
+
+            items[rear] = item;
+            rear = (rear + 1) % items.Length;
+            incr++;
+            count++;
+        }
+
+        public int Dequeue()
+        {
+            if (count == 0)
+            {
+                throw new Exception("Queue is underflow");
+            }
+
+            var item = items[front];
+            front = (front + 1) % items.Length;
+            count--;
+
+            return item;
+        }
+
+        public void Print()
+        {
+            int max = incr >= items.Length ? items.Length:rear;
+
+            for (int i = front; i < max; i++)
+            {
+                Console.WriteLine(items[i]);
+            }
+
+            if (incr >= items.Length)
+            {
+                for (int i = incr - items.Length; i > 0; i--)
+                {
+                    Console.WriteLine(items[i - 1]);
+                }
+            }
+
+        }
+
+        public int Peek()
+        {
+            if (count == 0)
+            {
+                throw new Exception("Queue is underflow");
+            }
+
+            return items[front];
+        }
+        #endregion
+    }
+
+    public class ReverseQueue
+    {
+        Stack<int> stack = new Stack<int>();
+        public Queue<int> Reverse(Queue<int> queue)
+        {
+            while (queue.Count != 0)
+            {
+                stack.Push(queue.Dequeue());
+            }
+
+            while (stack.Count != 0)
+            {
+                queue.Enqueue(stack.Pop());
+            }
+
+            return queue;
+        }
 
     }
 }
