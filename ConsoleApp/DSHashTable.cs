9482c266b04842476a9451eb69b5435c44e83955
Harsha Vardhan Reddy <harshasanapu100@gmail.com>
2021-05-14 22:22:27 +0530

    Add DSHashTable, CustomHashTable classes and renamed Main method in DSQueueArray class

---
+++ ConsoleApp/DSHashTable.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using CustomLibrary;
+
+namespace ConsoleApp
+{
+    class DSHashTable
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                #region CustomHashTable
+                Console.ForegroundColor = ConsoleColor.DarkYellow;
+                Console.WriteLine("Implementing the hash table contains key as int and value as string with the capacity of 5 elements");
+                Console.ForegroundColor = ConsoleColor.White;
+                CustomHashTable customHashTable = new CustomHashTable(5);
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Pushing the elements to hash table");
+                Console.ForegroundColor = ConsoleColor.White;
+
+                customHashTable.Add(0, "Harsha");
+                customHashTable.Add(1, "Amar");
+                customHashTable.Add(2, "Prudhvi");
+                customHashTable.Add(3, "Ashok");
+                customHashTable.Add(4, "Raja");
+
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Printing the elements");
+                Console.ForegroundColor = ConsoleColor.White;
+                customHashTable.Print();
+
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Pushing somemore elements to hash table");
+                Console.ForegroundColor = ConsoleColor.White;
+
+                customHashTable.Add(12, "Pavani");
+                customHashTable.Add(19, "Pallavi");
+                customHashTable.Add(16, "Sravanti");
+                customHashTable.Add(15, "Madhavi");
+
+                Console.WriteLine();
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Printing the elements");
+                Console.ForegroundColor = ConsoleColor.White;
+                customHashTable.Print();
+
+                Console.WriteLine();
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Get the item by key");
+                Console.ForegroundColor = ConsoleColor.White;
+                string result = customHashTable.Get(2);
+                Console.WriteLine("key : {0} value: {1}", 2, result);
+                result = customHashTable.Get(19);
+                Console.WriteLine("key : {0} value: {1}", 19, result);
+                result = customHashTable.Get(4);
+                Console.WriteLine("key : {0} value: {1}", 4, result);
+                result = customHashTable.Get(7);
+                Console.WriteLine("key : {0} value: {1}", 7, result);
+
+                Console.WriteLine();
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Remove the item by key");
+                Console.ForegroundColor = ConsoleColor.White;
+
+                Console.WriteLine("Removed key is 4");
+                customHashTable.Remove(4);
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Printing the elements");
+                Console.ForegroundColor = ConsoleColor.White;
+                customHashTable.Print();
+
+                Console.WriteLine();
+                Console.WriteLine("Removed key is 15");
+                customHashTable.Remove(15);
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Printing the elements");
+                Console.ForegroundColor = ConsoleColor.White;
+                customHashTable.Print();
+
+                Console.ReadKey();
+                #endregion
+            }
+            catch (Exception ex)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(ex.Message);
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+        }
+    }
+}
---
+++ CustomLibrary/CustomHashTable.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomLibrary
+{
+    public class CustomHashTable
+    {
+        #region private fileds
+        private class Entry
+        {
+            public int key;
+            public string value;
+            public Entry(int key, string value)
+            {
+                this.key = key;
+                this.value = value;
+            }
+        }
+
+        private LinkedList<Entry>[] entries;
+        #endregion
+
+        #region constructor
+        public CustomHashTable(int capacity)
+        {
+            this.entries = new LinkedList<Entry>[5];
+        }
+        #endregion
+
+        #region public methods
+        public void Add(int key, string value)
+        {
+            var index = hash(key);
+            if (entries[index] == null)
+            {
+                entries[index] = new LinkedList<Entry>();
+            }
+
+            var bucket = entries[index];
+            foreach (Entry entry in bucket)
+            {
+                if (entry.key == key)
+                {
+                    throw new Exception("Key already exists");
+                }
+            }
+
+            entries[index].AddLast(new Entry(key, value));
+        }
+
+        public string Get(int key)
+        {
+            var index = hash(key);
+            var bucket = entries[index];
+
+            if (bucket != null)
+            {
+                foreach (Entry entry in bucket)
+                {
+                    if (entry.key == key)
+                    {
+                        return entry.value;
+                    }
+                }
+            }
+
+            return null;
+        }
+
+        public void Remove(int key)
+        {
+            var index = hash(key);
+            var bucket = entries[index];
+
+            if (bucket == null)
+            {
+                throw new Exception("Key is not exists");
+            }
+
+            foreach (Entry entry in bucket)
+            {
+                if (entry.key == key)
+                {
+                    bucket.Remove(entry);
+                    return;
+                }
+            }
+
+            throw new Exception("Key is not exists");
+        }
+
+        public void Print()
+        {
+            foreach (LinkedList<Entry> listEntry in entries)
+            {
+                if (listEntry != null)
+                {
+                    foreach (Entry entry in listEntry)
+                    {
+                        Console.Write(entry.key + " " + entry.value + "\t");
+                    }
+                    Console.WriteLine();
+                }
+            }
+        }
+        #endregion
+
+        #region private methods
+        private int hash(int key)
+        {
+            return key % entries.Length;
+        }
+
+        #endregion
+
+    }
+}
