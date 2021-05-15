88e8f2143d3b607caacb17198e1a5af5432cc881
Harsha Vardhan Reddy <harshasanapu100@gmail.com>
2021-05-15 20:04:52 +0530

    Add PairsWithDiff, IndicesOfTwoNumbersWithTargetSum and CustomHashTableUsingLinearProbing classes

--- ConsoleApp/DSHashTable.cs
+++ ConsoleApp/DSHashTable.cs
@@ -9,52 +9,52 @@ namespace ConsoleApp
         {
             try
             {
-                #region CustomHashTable
+                #region CustomHashTableUsingChaining
                 Console.ForegroundColor = ConsoleColor.DarkYellow;
-                Console.WriteLine("Implementing the hash table contains key as int and value as string with the capacity of 5 elements");
+                Console.WriteLine("Implementing the hash table contains key as int and value as string with the capacity of 5 elements using chaining");
                 Console.ForegroundColor = ConsoleColor.White;
-                CustomHashTable customHashTable = new CustomHashTable(5);
+                CustomHashTableUsingChaining customHashTableUsingChaining = new CustomHashTableUsingChaining(5);
                 Console.ForegroundColor = ConsoleColor.Green;
                 Console.WriteLine("Pushing the elements to hash table");
                 Console.ForegroundColor = ConsoleColor.White;
 
-                customHashTable.Add(0, "Harsha");
-                customHashTable.Add(1, "Amar");
-                customHashTable.Add(2, "Prudhvi");
-                customHashTable.Add(3, "Ashok");
-                customHashTable.Add(4, "Raja");
+                customHashTableUsingChaining.Add(0, "Harsha");
+                customHashTableUsingChaining.Add(1, "Amar");
+                customHashTableUsingChaining.Add(2, "Prudhvi");
+                customHashTableUsingChaining.Add(3, "Ashok");
+                customHashTableUsingChaining.Add(4, "Raja");
 
                 Console.ForegroundColor = ConsoleColor.Green;
                 Console.WriteLine("Printing the elements");
                 Console.ForegroundColor = ConsoleColor.White;
-                customHashTable.Print();
+                customHashTableUsingChaining.Print();
 
                 Console.ForegroundColor = ConsoleColor.Green;
                 Console.WriteLine("Pushing somemore elements to hash table");
                 Console.ForegroundColor = ConsoleColor.White;
 
-                customHashTable.Add(12, "Pavani");
-                customHashTable.Add(19, "Pallavi");
-                customHashTable.Add(16, "Sravanti");
-                customHashTable.Add(15, "Madhavi");
+                customHashTableUsingChaining.Add(12, "Pavani");
+                customHashTableUsingChaining.Add(19, "Pallavi");
+                customHashTableUsingChaining.Add(16, "Sravanti");
+                customHashTableUsingChaining.Add(15, "Madhavi");
 
                 Console.WriteLine();
                 Console.ForegroundColor = ConsoleColor.Green;
                 Console.WriteLine("Printing the elements");
                 Console.ForegroundColor = ConsoleColor.White;
-                customHashTable.Print();
+                customHashTableUsingChaining.Print();
 
                 Console.WriteLine();
                 Console.ForegroundColor = ConsoleColor.Green;
                 Console.WriteLine("Get the item by key");
                 Console.ForegroundColor = ConsoleColor.White;
-                string result = customHashTable.Get(2);
+                string result = customHashTableUsingChaining.Get(2);
                 Console.WriteLine("key : {0} value: {1}", 2, result);
-                result = customHashTable.Get(19);
+                result = customHashTableUsingChaining.Get(19);
                 Console.WriteLine("key : {0} value: {1}", 19, result);
-                result = customHashTable.Get(4);
+                result = customHashTableUsingChaining.Get(4);
                 Console.WriteLine("key : {0} value: {1}", 4, result);
-                result = customHashTable.Get(7);
+                result = customHashTableUsingChaining.Get(7);
                 Console.WriteLine("key : {0} value: {1}", 7, result);
 
                 Console.WriteLine();
@@ -63,19 +63,19 @@ namespace ConsoleApp
                 Console.ForegroundColor = ConsoleColor.White;
 
                 Console.WriteLine("Removed key is 4");
-                customHashTable.Remove(4);
+                customHashTableUsingChaining.Remove(4);
                 Console.ForegroundColor = ConsoleColor.Green;
                 Console.WriteLine("Printing the elements");
                 Console.ForegroundColor = ConsoleColor.White;
-                customHashTable.Print();
+                customHashTableUsingChaining.Print();
 
                 Console.WriteLine();
                 Console.WriteLine("Removed key is 15");
-                customHashTable.Remove(15);
+                customHashTableUsingChaining.Remove(15);
                 Console.ForegroundColor = ConsoleColor.Green;
                 Console.WriteLine("Printing the elements");
                 Console.ForegroundColor = ConsoleColor.White;
-                customHashTable.Print();
+                customHashTableUsingChaining.Print();
                 #endregion
 
                 #region CharacterFinder
@@ -124,6 +124,85 @@ namespace ConsoleApp
                 Console.WriteLine();
                 int value = mostFrequentFinder.FindMostFrequentElement();
                 Console.WriteLine("Most frequented element in the array is: {0}", value);
+                #endregion
+
+                #region PairsWithDiff
+                Console.WriteLine();
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Count the pairs with the given difference");
+                Console.ForegroundColor = ConsoleColor.White;
+
+                PairsWithDiff pairsWithDiff = new PairsWithDiff(7);
+                int difference = 2;
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Difference is: {0}", difference);
+                Console.ForegroundColor = ConsoleColor.White;
+
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Pushing the 1, 7, 5, 9, 2, 12, 3 elements to array");
+                pairsWithDiff.items[0] = 1;
+                pairsWithDiff.items[1] = 7;
+                pairsWithDiff.items[2] = 5;
+                pairsWithDiff.items[3] = 9;
+                pairsWithDiff.items[4] = 2;
+                pairsWithDiff.items[5] = 12;
+                pairsWithDiff.items[6] = 3;
+                Console.ForegroundColor = ConsoleColor.White;
+
+                value = pairsWithDiff.CountPairsWithDiff(difference);
+                Console.WriteLine("Total pairs with the given difference {0} is {1}", difference, value);
+                #endregion
+
+                #region CustomHashTableUsingLinearProbing
+                Console.ForegroundColor = ConsoleColor.DarkYellow;
+                Console.WriteLine("Implementing the hash table contains key as int and value as string with the capacity of 5 elements using chaining");
+                Console.ForegroundColor = ConsoleColor.White;
+                CustomHashTableUsingLinearProbing customHashTableUsingLinearProbing = new CustomHashTableUsingLinearProbing(5);
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Pushing the elements to hash table");
+                Console.ForegroundColor = ConsoleColor.White;
+
+                customHashTableUsingLinearProbing.Add(0, "Harsha");
+                customHashTableUsingLinearProbing.Add(1, "Amar");
+                customHashTableUsingLinearProbing.Add(2, "Prudhvi");
+                customHashTableUsingLinearProbing.Add(3, "Ashok");
+                customHashTableUsingLinearProbing.Add(4, "Raja");
+
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Printing the elements");
+                Console.ForegroundColor = ConsoleColor.White;
+                customHashTableUsingLinearProbing.Print();
+
+                Console.WriteLine();
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Get the item by key");
+                Console.ForegroundColor = ConsoleColor.White;
+                result = customHashTableUsingLinearProbing.Get(2);
+                Console.WriteLine("key : {0} value: {1}", 2, result);
+                result = customHashTableUsingLinearProbing.Get(3);
+                Console.WriteLine("key : {0} value: {1}", 3, result);
+                result = customHashTableUsingLinearProbing.Get(4);
+                Console.WriteLine("key : {0} value: {1}", 4, result);
+
+                Console.WriteLine();
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Remove the item by key");
+                Console.ForegroundColor = ConsoleColor.White;
+
+                Console.WriteLine("Removed key is 4");
+                customHashTableUsingLinearProbing.Remove(4);
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Printing the elements");
+                Console.ForegroundColor = ConsoleColor.White;
+                customHashTableUsingLinearProbing.Print();
+
+                Console.WriteLine();
+                Console.WriteLine("Removed key is 2");
+                customHashTableUsingLinearProbing.Remove(2);
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Printing the elements");
+                Console.ForegroundColor = ConsoleColor.White;
+                customHashTableUsingLinearProbing.Print();
                 #endregion
 
                 Console.ReadKey();
--- CustomLibrary/CustomHashTableUsingChaining.cs
+++ CustomLibrary/CustomHashTableUsingChaining.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 
 namespace CustomLibrary
 {
-    public class CustomHashTable
+    public class CustomHashTableUsingChaining
     {
         #region private fileds
         private class Entry
@@ -21,7 +21,7 @@ namespace CustomLibrary
         #endregion
 
         #region constructor
-        public CustomHashTable(int capacity)
+        public CustomHashTableUsingChaining(int capacity)
         {
             this.entries = new LinkedList<Entry>[5];
         }
@@ -30,7 +30,7 @@ namespace CustomLibrary
         #region public methods
         public void Add(int key, string value)
         {
-            var index = hash(key);
+            var index = Hash(key);
             if (entries[index] == null)
             {
                 entries[index] = new LinkedList<Entry>();
@@ -50,7 +50,7 @@ namespace CustomLibrary
 
         public string Get(int key)
         {
-            var index = hash(key);
+            var index = Hash(key);
             var bucket = entries[index];
 
             if (bucket != null)
@@ -69,7 +69,7 @@ namespace CustomLibrary
 
         public void Remove(int key)
         {
-            var index = hash(key);
+            var index = Hash(key);
             var bucket = entries[index];
 
             if (bucket == null)
@@ -106,7 +106,7 @@ namespace CustomLibrary
         #endregion
 
         #region private methods
-        private int hash(int key)
+        private int Hash(int key)
         {
             return key % entries.Length;
         }
@@ -195,9 +195,9 @@ namespace CustomLibrary
                 }
             }
 
-            foreach (KeyValuePair<int,int> keyValuePair in keyValuePairs)
+            foreach (KeyValuePair<int, int> keyValuePair in keyValuePairs)
             {
-                if(keyValuePair.Value > max)
+                if (keyValuePair.Value > max)
                 {
                     max = keyValuePair.Value;
                     result = keyValuePair.Key;
@@ -206,5 +206,184 @@ namespace CustomLibrary
 
             return result;
         }
+    }
+
+    public class PairsWithDiff
+    {
+        public int[] items;
+        public PairsWithDiff(int capacity)
+        {
+            this.items = new int[capacity];
+        }
+
+        public int CountPairsWithDiff(int difference)
+        {
+            HashSet<int> hashSet = new HashSet<int>();
+            int count = 0;
+
+            foreach (int item in items)
+            {
+                hashSet.Add(item);
+            }
+
+            foreach (int item in items)
+            {
+                if (hashSet.Contains(item + difference))
+                {
+                    count++;
+                }
+
+                if (hashSet.Contains(item - difference))
+                {
+                    count++;
+                }
+
+                hashSet.Remove(item);
+            }
+
+            return count;
+        }
+    }
+
+    public class IndicesOfTwoNumbersWithTargetSum
+    {
+        public int[] items;
+        public IndicesOfTwoNumbersWithTargetSum(int capacity)
+        {
+            this.items = new int[capacity];
+        }
+
+        public void GetIndicesOfTwoNumbersWithTargetSum(int target)
+        {
+            Dictionary<int, int> keyValuePairs = new Dictionary<int, int>();
+
+            for (int i = 0; i < items.Length; i++)
+            {
+                int complement = target - items[i];
+                if (keyValuePairs.ContainsKey(complement))
+                {
+                    Console.WriteLine(keyValuePairs[complement] + " " + i);
+                }
+                keyValuePairs.Add(items[i], i);
+            }
+        }
+    }
+
+    public class CustomHashTableUsingLinearProbing
+    {
+        #region private fileds
+        private class Entry
+        {
+            public int key;
+            public string value;
+
+            public Entry(int key, string value)
+            {
+                this.key = key;
+                this.value = value;
+            }
+        }
+        private Entry[] entries;
+        private int count;
+        #endregion
+
+        #region constructor
+        public CustomHashTableUsingLinearProbing(int capacity)
+        {
+            this.entries = new Entry[capacity];
+        }
+        #endregion
+
+        #region public methods
+        public void Add(int key, string value)
+        {
+            var entry = GetEntry(key);
+            if (entry != null)
+            {
+                throw new Exception("Key already exists");
+            }
+
+            if (IsFull())
+            {
+                throw new Exception("Hash table is overflow");
+            }
+
+            entries[GetIndex(key)] = new Entry(key, value);
+            count++;
+        }
+
+        public string Get(int key)
+        {
+            var entry = GetEntry(key);
+            return entry != null ? entry.value : null;
+        }
+
+        public void Remove(int key)
+        {
+            var index = GetIndex(key);
+            if (index == -1 || entries[index] == null)
+            {
+                return;
+            }
+
+            entries[index] = null;
+            count--;
+        }
+
+        public void Print()
+        {
+            for (int i = 0; i < entries.Length; i++)
+            {
+                if (entries[i] != null)
+                {
+                    Console.WriteLine(entries[i].key + " " + entries[i].value);
+                }
+                else
+                {
+                    count = count - 1;
+                }
+            }
+        }
+        #endregion
+
+        #region private methods
+
+        private Entry GetEntry(int key)
+        {
+            var index = GetIndex(key);
+            return index >= 0 ? entries[index] : null;
+        }
+
+        private int GetIndex(int key)
+        {
+            int steps = 0;
+            while (steps < entries.Length)
+            {
+                int index = Index(key, steps++);
+                var entry = entries[index];
+                if (entry == null || entry.key == key)
+                {
+                    return index;
+                }
+            }
+
+            return -1;
+        }
+
+        private int Index(int key, int i)
+        {
+            return (Hash(key) + i) % entries.Length;
+        }
+
+        private int Hash(int key)
+        {
+            return key % entries.Length;
+        }
+
+        private bool IsFull()
+        {
+            return count == entries.Length;
+        }
+        #endregion
     }
 }
